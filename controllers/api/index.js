2078875bb208b215d5aa45904fb804a583c446c8
aaloksikand <aalok.sikand@gmail.com>
2022-07-10 16:27:10 -0700

    Copied, pasted and revised controllers\api\index.js from 28-Stu_Mini-Project and incorporated it into api\index.js file to test GET movieroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const movieRoutes = require('./movieRoutes');
 
-//router.use('/users', userRoutes);
-//router.use('/projects', projectRoutes);
+router.use('/movies', movieRoutes);
 
 module.exports = router;
