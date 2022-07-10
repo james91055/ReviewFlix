5a0a6236030433a6d5dba7165839fb2f87ddd073
DWKanas <dkanas0122@gmail.com>
2022-07-10 16:42:40 -0700

    changed home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const {Movie} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const movieData = await Movie.findAll({
+      
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const movies = movieData.map((movie) => movie.get({ plain: true }));
 
     res.render('homepage', {
-      users,
+      movies,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
