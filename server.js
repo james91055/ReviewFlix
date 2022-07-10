23235cd67e7688bbcdd9f833eb3f1b1eaca84478
aaloksikand <aalok.sikand@gmail.com>
2022-07-10 16:42:20 -0700

    Commented out sess from server.js to run test of GET routes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3001;
 //   })
 // };
 
-app.use(session(sess));
+//app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
