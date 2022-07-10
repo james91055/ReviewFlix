338e63a415039ad75c8557fbc9d4e3014b67a869
aaloksikand <aalok.sikand@gmail.com>
2022-07-10 16:21:44 -0700

    Copied, pasted and revised controllers/index.js  of 28-Stu_Mini-Project and incorporated it into controllers\index.js file

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const movieRoutes = require('./api/movieRoutes');
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+router.use('/api', movieRoutes);
 
 module.exports = router;
