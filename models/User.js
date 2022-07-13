9a9249bf491a7b7188ee33011d85a19d35985f80
guitarkeegan <keegananglim@gmail.com>
2022-07-13 13:21:42 -0700

    add default user image

--- models/User.js
+++ models/User.js
@@ -37,6 +37,8 @@ User.init(
     },
     profileUrl: {
       type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2019%2F07%2Fgettyimages-81692747-2000.jpg&q=60"
     }
   },
   {
