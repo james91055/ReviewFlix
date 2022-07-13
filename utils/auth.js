062f9b642db682f41a85686146fe7e26440e8c1e
James <james91055@gmail.com>
2022-07-13 14:38:38 -0700

    update comments for seeds/utils/views folder

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
 const withAuth = (req, res, next) => {
-    
+    // Set a middleware to allow user with loggedIn status pass, and send the user are not logged in to login page
     if (!req.session.loggedIn) {
       console.log("You cannot pass")
       res.redirect('/login');
