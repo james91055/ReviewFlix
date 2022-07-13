355faf1df7f9fce806a9b8fa3736af0465ddde87
guitarkeegan <keegananglim@gmail.com>
2022-07-13 14:34:22 -0700

    adding comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const { Movie, Review, User } = require("../models");
-
+// Find all available movies from the movie model, and display them on screen. Each object includes an image and a description.
 router.get("/", async (req, res) => {
   try {
+    // find all movies from table
     const movieData = await Movie.findAll({});
-
+    // for each movie object in the array, 'get' the essential information.
     const movies = movieData.map((movie) => movie.get({ plain: true }));
-
+// render the movieList.handlebars page. Pass in movies array and the logged in parameter of the req.session object.
     res.render("movieList", {
       movies,
       loggedIn: req.session.loggedIn,
@@ -15,21 +16,23 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+// The login page will allow users to either sign up or login to the site.
 router.get("/login", (req, res) => {
+  // user is redirected to the homepage if they are already logged in.
   if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
-
+// if user is not already logged in, render the login page.
   res.render("login");
 });
-// TODO: double check against model
-
+// User can view the all of the information on the movie that they selected. If they are logged in, they will be able to leave a review.
 router.get("/movie/:id", async (req, res) => {
   try {
+    // find the movie by it's primary key from the req.params.id
     const movieData = await Movie.findByPk(req.params.id, {
       include: {
+        // display any previous reviews
         model: Review,
         attributes: ["content", "user_id"],
         include: [{ model: User }],
